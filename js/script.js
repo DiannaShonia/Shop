f8d8e15a78b5dc450a19313aca2980f7af31012b
Lily <DianaShonia999@gmail.com>
2021-01-18 03:34:09 +0400

    added quantity spinner

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let num = 0;
 let cart = [];
 
 
+
 basket.addEventListener('click', function() {
     if (basketDropdown.style.display == 'block'){
         basketDropdown.style.display = 'none'
@@ -28,40 +29,83 @@ for (let i = 0; i < addToBasketBtn.length; i++) {
             id: this.dataset.id,
             title: this.dataset.title,
             price: this.dataset.price,
-            quantity: '',
         }
 
 
         cart.push(product)
         
         let item = document.createElement('li');
-        let deleteItem = document.createElement('div');
-        total += parseInt(product.price)
-
-
-        
         item.textContent = product.title + " " + product.price;
+
+        let deleteItem = document.createElement('div');
         deleteItem.classList.add('delete-item')
+
+        price = parseInt(product.price)
+        total += price;
+        
+
+        // product quantity number spinner
+
+        let value = 1;
+        let quantityRow = document.createElement('div');
+        quantityRow.classList.add('quantity-row');
+        let addQuantity = document.createElement('div');
+        addQuantity.classList.add('add-quantity')
+        let quantity = document.createElement('div');
+        quantity.classList.add('quantity');
+        let minusQuantity = document.createElement('div');
+        minusQuantity.classList.add('minus-quantity');
+        
+        quantityRow.appendChild(minusQuantity);
+        quantityRow.appendChild(quantity);
+        quantityRow.appendChild(addQuantity);
+        quantity.innerText = '1';
+
+
+        addQuantity.addEventListener('click', function (){
+            value++;
+            quantity.innerText = value;
+
+            total+=price;
+            totalPrice.textContent = 'Total: ' + total   + '$';
+
+            
+        })
+
+        minusQuantity.addEventListener('click', function(){
+            if (value > 1){
+                value--;
+                quantity.innerText = value;
+                total-=price;
+                totalPrice.textContent = 'Total: ' + total   + '$';
+            }
+        })
+
+
+        // adding products to html 
 
         cart.forEach(function(){
 
-            totalPrice.textContent = 'Total: ' + total + '$';
+            totalPrice.textContent = 'Total: ' + total   + '$';
 
             list.appendChild(item)
+            item.appendChild(quantityRow)
             item.appendChild(deleteItem)
             basketDropdown.appendChild(totalPrice)
 
         })
 
+
+
         deleteItem.addEventListener('click', function(){
-            total -= parseInt(product.price)
-            totalPrice.textContent = 'Total: ' + total + '$';
+            total -=  value * price
+            totalPrice.textContent = 'Total: ' + total   + '$';
+
             list.removeChild(item)
 
             num--;
             counter.textContent = num;
-
-        })         
+        })     
         
     })
 }
